aa55cce29199d6edf03aac0bf8813a84667c1221
Finlay Thompson <finlay@dragonfly.co.nz>
2016-05-13 16:56:55 +1200

    Update to gorbachev base

--- ubuntu/gorbachev-base/r_requirements.r
+++ ubuntu/gorbachev-base/r_requirements.r
@@ -5,8 +5,10 @@ options(defaultPackages = c(old, "MASS"), repos = r)
 packages <- c(
     "shapefiles", "foreign", "sp", "grid", "lattice", "rgeos", "RColorBrewer",
     "maptools", "RPostgreSQL", "knitr", "rjson", "pander", "ggplot2", "dplyr",
-    "tables", "data.table", "tidyr", "gridExtra", "rjags","R2jags","reshape2","mapproj",
-    "cplm","lme4"
+    "tables", "data.table", "tidyr", "gridExtra", "rjags", "R2jags", "reshape2", 
+    "mapproj", "cplm", "lme4", 'xtable', 'plyr', 'lubridate', 'scales', 'rgdal', 
+    'ggmap', 'Cairo', 'maps', 'Matching', 'BenfordTests', 'genoud', 'tools', 
+    'utils', 'rgenoud', 'broom','purrr'
 )
 
 for (p in packages) {
@@ -14,3 +16,8 @@ for (p in packages) {
         install.packages(p)
     }
 }
+
+# INLA has its own repo...
+if (!require("INLA", character.only=TRUE)) {
+        install.packages("INLA", repos="http://www.math.ntnu.no/inla/R/testing")
+    }
