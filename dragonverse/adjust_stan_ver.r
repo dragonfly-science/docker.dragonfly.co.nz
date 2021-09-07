2e74f2bf6f303c886df9429ca50baad6246d34ea
Edward Abraham <edward@dragonfly.co.nz>
2021-09-08 07:21:00 +1200

    Try again

--- dragonverse/adjust_stan_ver.r
+++
@@ -1,6 +0,0 @@
-#remove.packages(c("StanHeaders","rstan",'RcppEigen'))
-
-install.packages("StanHeaders", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-install.packages("brms")
-
