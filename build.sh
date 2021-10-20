d5de2d15fb7f4e3b16de07d64cafffef4c3b43e5
Edward Abraham <edward@dragonfly.co.nz>
2021-10-20 20:43:13 +1300

    Remove AWS for now

--- build.sh
+++ build.sh
@@ -6,15 +6,15 @@ export DATE=$(date +%Y-%m-%d)
 
 #make dragonfly
 
-AWS_PROFILE=dragonfly-ecr
-AWS_REGION=us-east-1
-
-aws configure --profile ${AWS_PROFILE} set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-aws configure --profile ${AWS_PROFILE} set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-aws configure --profile ${AWS_PROFILE} set region ${AWS_REGION}
-eval $(aws ecr get-login --profile ${AWS_PROFILE} | sed 's| -e none||')
-
-make aws
+#AWS_PROFILE=dragonfly-ecr
+#AWS_REGION=us-east-1
+#
+#aws configure --profile ${AWS_PROFILE} set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+#aws configure --profile ${AWS_PROFILE} set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+#aws configure --profile ${AWS_PROFILE} set region ${AWS_REGION}
+#eval $(aws ecr get-login --profile ${AWS_PROFILE} | sed 's| -e none||')
+#
+#make aws
 
 DOCKERHUB_USERNAME=deployhub1
 docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
