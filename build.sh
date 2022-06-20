5f2c8b6c2c8bb320a9b6a92336e2abd12238a4ae
Richard Mansfield <richard@dragonfly.co.nz>
2022-06-20 12:40:15 +1200

    Update mpi templates

--- build.sh
+++ build.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-set -e
-
+set -eu
 export DATE=$(date +%Y-%m-%d)
-
-make dragonfly
-
-AWS_PROFILE=dragonfly-ecr
-AWS_REGION=us-east-1
-
-aws configure --profile ${AWS_PROFILE} set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-aws configure --profile ${AWS_PROFILE} set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-aws configure --profile ${AWS_PROFILE} set region ${AWS_REGION}
-eval $(aws ecr get-login --profile ${AWS_PROFILE} | sed 's| -e none||')
-
-make aws
-
-DOCKERHUB_USERNAME=deployhub1
-docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
-
+cp /input/MPI-Latex-Templates/mpi-latex-templates_1.42_all.deb dragonfly-reports/
+docker login --username deployhub1 --password ${DOCKERHUB_PASSWORD}
 make dockerhub
-
-exit $?
