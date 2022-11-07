4ff6429cda078c2b4d17de1aef4daf4a1058b349
Richard Mansfield <richard@dragonfly.co.nz>
2022-11-08 09:35:13 +1300

    Change timezone for image tag

--- build.sh
+++ build.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-set -e
+set -eu
 
+export TZ="Pacific/Auckland"
 export DATE=$(date +%Y-%m-%d)
-
-#make dragonfly
-
-#AWS_PROFILE=dragonfly-ecr
-#AWS_REGION=us-east-1
-#
-#aws configure --profile ${AWS_PROFILE} set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-#aws configure --profile ${AWS_PROFILE} set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-#aws configure --profile ${AWS_PROFILE} set region ${AWS_REGION}
-#eval $(aws ecr get-login --profile ${AWS_PROFILE} | sed 's| -e none||')
-#
-#make aws
 
 cp /input/MPI-Latex-Templates/mpi-latex-templates_1.51_all.deb dragonfly-reports/
 cp /input/Dragonfly-Latex-Templates/dragonfly-latex-templates_2.03_all.deb dragonfly-reports/
 
 DOCKERHUB_USERNAME=deployhub1
 docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
-
 make dockerhub
-
-exit $?
