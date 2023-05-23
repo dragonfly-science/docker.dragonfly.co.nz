5101ed486e6907570c50406fe38d0c1bdadc13fd
Edward Abraham <edward@dragonfly.co.nz>
2023-05-24 09:49:41 +1200

    Update dragonfly reports; and move forward all the R packages

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ export DATE=$(date +%Y-%m-%d)
 #make aws
 
 cp /input/MPI-Latex-Templates/mpi-latex-templates_1.51_all.deb dragonfly-reports/
+cp /input/Dragonfly-Latex-Templates/dragonfly-latex-templates_2.03_all.deb dragonfly-reports/
 
 DOCKERHUB_USERNAME=deployhub1
 docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
