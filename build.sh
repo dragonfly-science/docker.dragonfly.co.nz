3a75403a0d92533cb97caf1dada48bb131d01215
Finlay Thompson <finlay@dragonfly.co.nz>
2025-05-05 13:05:02 +1200

    Updated MPI latex templates to 1.62

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -eu
 export TZ="Pacific/Auckland"
 export DATE=$(date +%Y-%m-%d)
 
-cp /input/MPI-Latex-Templates/mpi-latex-templates_1.61_all.deb dragonfly-reports/
+cp /input/MPI-Latex-Templates/mpi-latex-templates_1.62_all.deb dragonfly-reports/
 cp /input/Dragonfly-Latex-Templates/dragonfly-latex-templates_2.03_all.deb dragonfly-reports/
 
 DOCKERHUB_USERNAME=deployhub1
